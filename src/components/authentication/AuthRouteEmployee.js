350e91302be3fcd43561c3a484e2be55d8a1c529
Jindra <svobodajindrich@centrum.cz>
2022-02-01 12:03:16 +0100

    auth

---
+++ src/components/authentication/AuthRouteEmployee.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react'
+import { Route, Redirect } from 'react-router-dom'
+import {USER_NAME_SESSION_ATTRIBUTE_ROLE} from '../authentication/AuthenticationService';
+
+class AuthRouteAdmin extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isAdminLoggedIn : false,
+            userRole : {}
+        }
+    }
+
+    componentDidMount() {
+        console.log(sessionStorage.getItem(USER_NAME_SESSION_ATTRIBUTE_ROLE))
+
+    }
+
+    render() {
+        if (sessionStorage.getItem(USER_NAME_SESSION_ATTRIBUTE_ROLE) === 'Employee'
+            || sessionStorage.getItem(USER_NAME_SESSION_ATTRIBUTE_ROLE) === 'Admin') {
+            return <Route {...this.props} />
+        } else {
+            return <Redirect to={{pathname: '/login', message: 'K pokračování nemáte potřebná oprávnění'}} />
+        }
+
+    }
+}
+
+export default AuthRouteAdmin;
